14386fe3df21822f2bd87b7d5a850945bbef12d2
AndreCuenca17 <andrecuencaechevarria@gmail.com>
2024-12-19 15:13:17 -0500

    Modifying run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=debug_mode)
